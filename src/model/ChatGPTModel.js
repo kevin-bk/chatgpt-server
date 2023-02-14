799a85d1ee8a8b6f014e6e625f88ca221f8f0c1c
truongnp <phitruong.nd2k@gmail.com>
2023-02-15 01:50:53 +0700

    update chatGPT model, and move proxy server to .env

--- src/model/ChatGPTModel.js
+++ src/model/ChatGPTModel.js
@@ -6,28 +6,15 @@ dotenv.config();
 class ChatGPT {
     constructor(conversationId, promptPrefix, chatGptLabel) {
         const clientOptions = {
-            // (Optional) Support for a reverse proxy for the completions endpoint (private API server).
-            // Warning: This will expose your `openaiApiKey` to a third-party. Consider the risks before using this.
-            reverseProxyUrl: 'https://chatgpt.pawan.krd/api/completions', // or https://chat.duti.tech/completions
-            // (Optional) Parameters as described in https://platform.openai.com/docs/api-reference/completions
+            reverseProxyUrl: process.env.PROXY_SERVER,
             modelOptions: {
-                // You can override the model name and any other parameters here.
-                model: process.env.CHATGPT_PLUS_MODEL,
+                model: process.env.CHATGPT_PLUS_MODEL
             },
-            // (Optional) Set custom instructions instead of "You are ChatGPT...".
             promptPrefix: promptPrefix,
-            // (Optional) Set a custom name for the user
-            // userLabel: 'User',
-            // (Optional) Set a custom name for ChatGPT
-            chatGptLabel: chatGptLabel,
-            // (Optional) Set to true to enable `console.debug()` logging
-            // debug: false,
+            chatGptLabel: chatGptLabel
         };
 
         const cacheOptions = {
-            // Options for the Keyv cache, see https://www.npmjs.com/package/keyv
-            // This is used for storing conversations, and supports additional drivers (conversations are stored in memory by default)
-            // For example, to use a JSON file (`npm i keyv-file`) as a database:
             store: new KeyvFile({ filename: `./src/database/caches/${conversationId}-cache.json` }),
         };
 
