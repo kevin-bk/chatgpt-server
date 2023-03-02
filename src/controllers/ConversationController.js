da38cae0d5d78bbda39ea8df3c36413773daa5c8
kevin <kevin@magestore.com>
2023-03-02 22:43:28 +0700

    fix bug get userLabel from controller

--- src/controllers/ConversationController.js
+++ src/controllers/ConversationController.js
@@ -2,7 +2,7 @@ import ConversationService from "../service/ConversationService.js";
 
 class ConversationController {
     async chat(req, res, next) {
-        ConversationService.chat(req.body.conversationId, req.body.message, req.body.userLabel)
+        ConversationService.chat(req.body.conversationId, req.body.message, req.body.fromUser)
             .then(response => {
                 res.json({
                     message: response
